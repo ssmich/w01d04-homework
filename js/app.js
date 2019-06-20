ecd095da0303edda8fb5d7ff358e119676ac9b30
Stacey Smich <ssmich@Staceys-MacBook-Pro.local>
2019-06-20 16:04:32 -0600

    Commit 1 - Verbal Questions

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// 1. Verbal Questions
+// 		1. What is the difference between a parameter and an argument?
+
+//		A parameter is the input variable for a function. An argument 
+//		is the value assigned to the parameter.
+
+//		2. Within a function, what is the difference between return 
+// 		   and console.log?
+
+//		Return specifies a value to be returned and terminates the code
+//		within a fuction. Console.log prints and does not return a value
+//		and is therefore undefined. 
+
+//		3. What are the implications of the ability of a function to 
+//		   return a value?
+
+//		Return must be included in the function as the last step.
+
+
